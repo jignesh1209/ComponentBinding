48cdbbab48f18cd406bd810fdf475a3fabb8208a
Jignesh Prajapati <jigneshprajapati22@gmail.com>
2019-12-27 22:25:45 +0530

    Event Emitter used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ComponentBinding';
+
+  serverElements = [{ type:'server', name: 'Testerver', content:'Just a test' }];
+
+  onServerAdded(serverData:{serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+  
+  onBluePrintAdded(bluPrintData:{serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluPrintData.serverName,
+      content: bluPrintData.serverContent
+    });
+  }
 }
